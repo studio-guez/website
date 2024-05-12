a08ea8cff23aaec7a8a165cb71c9c05a682b8051
azertypow <azertypow@gmail.com>
2024-05-12 22:17:28 +0200

    faet: init content

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
         studio guez
 
         site en maintenance
+
+        studio@guez.ch ?
     </section>
 </template>
 
@@ -26,5 +28,13 @@ const props = defineProps<{
 
 <style lang="scss" scoped >
 .v-index {
+    font-family: Arial, sans-serif;
+    color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: black;
+    width: 100%;
+    height: 100%;
 }
 </style>
