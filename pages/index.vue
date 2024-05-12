74388a46e26e5b95ddb06a0e40c163a10acf6494
azertypow <azertypow@gmail.com>
2024-05-12 19:56:21 +0200

    chore: github action

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <section
+        class="v-index"
+    >
+        studio guez
+
+        site en maintenance
+    </section>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+    message?: string
+}>()
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-index {
+}
+</style>
