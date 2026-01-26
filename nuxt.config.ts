769384d0125a78b8a479cccd863d496a1f82afae
azertypow <nico@villa1203.ch>
2026-01-26 11:06:40 +0100

    feat: set up static site generation and disable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/style/_main.scss'],
+  app: {
+    baseURL: '/',
+  },
+  ssr: false,
+  nitro: {
+    output: {
+      publicDir: 'docs',
+    },
+  }
 })
